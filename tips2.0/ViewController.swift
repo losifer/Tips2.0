63d2caf4923eda6ac94448bd6116553cf2c457ce
losifer <carloscanash@gmail.com>
2015-06-29 15:38:53 -0700

    first commit

---
+++ tips2.0/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  tips2.0
+//
+//  Created by Carlos Canas on 6/29/15.
+//  Copyright (c) 2015 Carlos Canas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Focus on the form field.
+        billField.delegate = self
+        self.billField.becomeFirstResponder()
+        
+        // Add corners to total view area
+        totalView.layer.cornerRadius = 4
+        
+        // Tip and total labels
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Reformat textfield to generate decimals
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        // println("Test")
+        
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = NSString(string: billField.text).doubleValue
+        
+        var tip = billAmount * tipPercentage
+        
+        var total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+}
+
